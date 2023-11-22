a4d790e49edfee3b83df9dcd8c426f649e6f291d
Iysah <yusufiysah@gmail>
2023-11-22 14:57:20 +0100

    updated styled component reload error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import StyledComponentsRegistry from "./lib/registry";
 
 
 export const metadata: Metadata = {
@@ -14,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <StyledComponentsRegistry>
+          {children}
+        </StyledComponentsRegistry>
+        </body>
     </html>
   )
 }
