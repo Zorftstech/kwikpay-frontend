5d8d050ae35c013effd788ddac4c1e73abf4ed41
Iysah <yusufiysah@gmail>
2023-11-22 22:15:33 +0100

    updated about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,6 +6,8 @@ import { Container, Heading, Flex } from '@chakra-ui/react'
 import { colors } from "@/styles/theme"
 import BlogCard from "@/components/AboutComponents/blogCard/index"
 import { blogs } from "@/components/AboutComponents/data"
+import Newsletter from "@/components/AboutComponents/newsletter/index"
+import Testimonials from "@/components/AboutComponents/testimonial/index"
 
 
 const About: React.FC = () => {
@@ -13,14 +15,18 @@ const About: React.FC = () => {
         <Layout>
             <AboutSection />
             <Portfolio />
-            <Container maxW='md' py='3rem' px='4rem' bg='#f2f7fc' color={`${colors.primaryColor}`}>
-                <Heading fontSize='54px' fontWeight='600' color={`${colors.primaryColor}`} textAlign='center' mt='7rem' mb='3rem'>Get to know KwikPay better with our blog</Heading>
+            <Container maxW='lg' pt='3rem' px='4rem' bg='#f2f7fc' color={`${colors.primaryColor}`}>
+                <Testimonials />
+            </Container>
+            <Container maxW='lg' py='3rem' px='4rem' bg='#f2f7fc' color={`${colors.primaryColor}`}>
+                <Heading fontSize='54px' fontWeight='600' color={`${colors.primaryColor}`} textAlign='center' mt='7rem' mb='3rem'>Learn from our experts</Heading>
                 <Flex justify='center' align='center' gap='1rem'>
                     {blogs.slice(0, 3).map((item) => (
                         <BlogCard id={item.id} title={item.title} image={item.image} text={item.text} time={item.time} /> 
                     ))}
                 </Flex>
             </Container>
+            <Newsletter />
         </Layout>
     )
 }
